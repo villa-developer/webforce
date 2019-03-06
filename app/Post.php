abac0cdb880f16072049de4619e5ef12456137d6
vagrant <vagrant@homestead>
2019-03-06 17:54:55 +0000

    mutator

--- app/Post.php
+++ app/Post.php
@@ -17,4 +17,10 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function setSlugAttribute($value)
+    {
+        $this->attributes['slug'] = str_slug($value, '-');
+    }
+
 }
